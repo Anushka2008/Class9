d8a5386d3b550e86b71e2745868f549c43e44585
Anushka2008 <102003187+Anushka2008@users.noreply.github.com>
2022-03-20 18:55:37 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var box
+function setup() {
+  createCanvas(400,400);
+  box=createSprite(200,200,50,50);
+}
+
+function draw() 
+{
+  background(30);
+  drawSprites()
+  if(keyIsDown(RIGHT_ARROW)){
+    box.x=box.x+5
+  }
+}
+
+
+
+
